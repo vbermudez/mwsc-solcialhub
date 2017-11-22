0d7298c1b58a786ef0f5f846a11ecc46d94f3846
vbermudez <vbermudez@xi-tek.com>
2017-11-22 16:01:21 +0100

    Heroku support

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const connect = require('connect');
 const serveStatic = require('serve-static');
 const publicDir = __dirname + '/public';
 
-connect().use( serveStatic(publicDir)).listen(8080, function() {
+connect().use( serveStatic(publicDir)).listen(process.env.PORT || 8080, function() {
     console.log('Server running on 8080...');
 });
